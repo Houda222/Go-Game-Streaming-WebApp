dcf843799cb22b257806f8ca88f8dfe0f2c94d7a
Houda222 <houda.ghallab@imt-atlantique.net>
2023-12-17 01:41:26 +0100

    listener previous next buttons

---
+++ static/game_manager.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var buttons = document.querySelectorAll('.btn-petit');
+
+    buttons.forEach(function (button) {
+        button.addEventListener('click', function () {
+            var form = document.getElementById('move');
+            var formData = new FormData(form);
+
+            formData.append('psw2', button.value);
+
+            var xhr = new XMLHttpRequest();
+            xhr.open('POST', '/game', true);
+
+            xhr.onload = function () {
+                // Handle the response if needed
+                console.log(xhr.responseText);
+            };
+
+            xhr.send(formData);
+        });
+    });
+});
