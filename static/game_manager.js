c1d1eda7facde21acf8ea5b124ec9d9821c0e17c
Houda222 <houda.ghallab@imt-atlantique.net>
2023-12-17 01:50:22 +0100

    test

--- static/game_manager.js
+++ static/game_manager.js
@@ -2,6 +2,7 @@
 
 const controls = document.getElementById("camera-feed");
 
+// for next previous initial final buttons
 document.addEventListener("DOMContentLoaded", function () {
     var buttons = document.querySelectorAll('.btn-petit');
 
@@ -22,5 +23,46 @@ document.addEventListener("DOMContentLoaded", function () {
 
             xhr.send(formData);
         });
+    });
+});
+
+
+// for undo button
+document.addEventListener("DOMContentLoaded", function () {
+    var undoButton = document.getElementById('undo-button');
+
+    undoButton.addEventListener('click', function () {
+        var undoForm = document.getElementById('undo-form');
+        var formData = new FormData(undoForm);
+
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', '/undo', true);
+
+        xhr.onload = function () {
+            // Handle the response if needed
+            console.log(xhr.responseText);
+        };
+
+        xhr.send(formData);
+    });
+});
+
+// change position
+document.addEventListener("DOMContentLoaded", function () {
+    var changePlaceButton = document.getElementById('change-place-button');
+
+    changePlaceButton.addEventListener('click', function () {
+        var changePlaceForm = document.getElementById('change');
+        var formData = new FormData(changePlaceForm);
+
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', '/change_place', true);
+
+        xhr.onload = function () {
+            // Handle the response if needed
+            console.log(xhr.responseText);
+        };
+
+        xhr.send(formData);
     });
 });
